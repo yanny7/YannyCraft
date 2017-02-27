108a520a72e61d45d70a50dcbea38b057d2ed246
BuildTools <unconfigured@null.spigotmc.org>
2017-02-27 20:10:37 +0100

    Removed unused import

--- src/me/noip/yanny/residence/ResidenceConfiguration.java
+++ src/me/noip/yanny/residence/ResidenceConfiguration.java
@@ -11,7 +11,6 @@ import org.bukkit.plugin.Plugin;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
