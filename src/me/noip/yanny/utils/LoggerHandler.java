d452a8892883a09e320410b993294a9b268c2518
BuildTools <unconfigured@null.spigotmc.org>
2017-03-16 00:05:00 +0100

    Colored console output

--- src/me/noip/yanny/utils/LoggerHandler.java
+++ src/me/noip/yanny/utils/LoggerHandler.java
@@ -6,6 +6,10 @@ import java.util.logging.Logger;
 
 public class LoggerHandler {
 
+    private static final String ANSI_RESET = "\u001B[0m";
+    private static final String ANSI_RED = "\u001B[1;31m";
+    private static final String ANSI_GREEN = "\u001B[1;32m";
+
     private Logger logger;
     private StringBuilder strBuilder = new StringBuilder();
 
@@ -15,13 +19,13 @@ public class LoggerHandler {
 
     public void logInfo(Class clazz, String msg) {
         strBuilder.setLength(0);
-        strBuilder.append('[').append(clazz.getSimpleName()).append("] ").append(msg);
+        strBuilder.append(ANSI_GREEN).append('[').append(clazz.getSimpleName()).append("] ").append(ANSI_RESET).append(msg);
         logger.info(strBuilder.toString());
     }
 
     public void logWarn(Class clazz, String msg) {
         strBuilder.setLength(0);
-        strBuilder.append('[').append(clazz.getSimpleName()).append("] ").append(msg);
+        strBuilder.append(ANSI_RED).append('[').append(clazz.getSimpleName()).append("] ").append(ANSI_RESET).append(msg);
         logger.warning(strBuilder.toString());
     }
 }
